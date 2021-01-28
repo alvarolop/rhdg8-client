808d6b88b27e215db22e1160bc20d15d6bd9bdd2
Alvaro Lopez Medina <alopezme@redhat.com>
2021-01-28 01:47:55 +0100

    Improved testing Java class, increased client version to 12.0.0.Final, prepared a Test class

--- hotrod-tester/src/main/java/com/alopezme/hotrodtester/controller/ReproducerTester.java
+++ hotrod-tester/src/main/java/com/alopezme/hotrodtester/controller/ReproducerTester.java
@@ -3,6 +3,8 @@ package com.alopezme.hotrodtester.controller;
 import com.alopezme.hotrodtester.model.Book;
 import com.alopezme.hotrodtester.utils.QueriesCacheManager;
 import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.commons.configuration.XMLStringConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,16 +16,62 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/reproducer")
 public class ReproducerTester {
 
+    @Autowired
+    RemoteCacheManager remoteCacheManager;
 
     @Autowired
     QueriesCacheManager queriesCacheManager;
+
+    String cacheName = "test-reproducer";
+//    String xml = String.format(
+//            "<infinispan>" +
+//            "   <cache-container>" +
+//            "       <distributed-cache name=\"%s\" mode=\"SYNC\" owners=\"1\" statistics=\"true\">" +
+//            "           <encoding>" +
+//            "               <key media-type=\"application/x-protostream\"/>" +
+//            "               <value media-type=\"application/x-protostream\"/>" +
+//            "           </encoding>" +
+//            "           <transaction mode=\"NONE\"/>" +
+//            "           <expiration lifespan=\"-1\" max-idle=\"-1\" interval=\"60000\"/>" +
+//            "           <memory storage=\"HEAP\"/>" +
+//            "           <indexing enabled=\"true\">" +
+//            "               <key-transformers/>" +
+//            "               <indexed-entities/>" +
+//            "           </indexing>" +
+//            "           <state-transfer enabled=\"false\" await-initial-transfer=\"false\"/>" +
+//            "           <partition-handling when-split=\"ALLOW_READ_WRITES\" merge-policy=\"REMOVE_ALL\"/>" +
+//            "       </distributed-cache>" +
+//            "   </cache-container>" +
+//            "</infinispan>", cacheName);
+
+    String xml = String.format(
+            "<infinispan>" +
+                    "   <cache-container>" +
+                    "       <distributed-cache name=\"%s\" mode=\"SYNC\" owners=\"1\" statistics=\"true\">" +
+                    "           <encoding>" +
+                    "               <key media-type=\"application/x-protostream\"/>" +
+                    "               <value media-type=\"application/x-protostream\"/>" +
+                    "           </encoding>" +
+//                    "           <transaction mode=\"NONE\"/>" +
+//                    "           <expiration lifespan=\"-1\" max-idle=\"-1\" interval=\"60000\"/>" +
+//                    "           <memory storage=\"HEAP\"/>" +
+//                    "           <indexing enabled=\"true\">" +
+//                    "               <key-transformers/>" +
+//                    "               <indexed-entities/>" +
+//                    "           </indexing>" +
+//                    "           <state-transfer enabled=\"false\" await-initial-transfer=\"false\"/>" +
+//                    "           <partition-handling when-split=\"ALLOW_READ_WRITES\" merge-policy=\"REMOVE_ALL\"/>" +
+                    "       </distributed-cache>" +
+                    "   </cache-container>" +
+                    "</infinispan>", cacheName);
 
     Logger logger = LoggerFactory.getLogger(QueriesTester.class);
 
     @GetMapping("test")
     public String loadBooksCacheTest() throws Exception{
 
-        RemoteCache<Integer, Book> cache = queriesCacheManager.getBookCache();
+        RemoteCache<Integer, Book> cache = queriesCacheManager.getManager().administration()
+                .getOrCreateCache(cacheName, new XMLStringConfiguration(xml));
 
         logger.info("\n--> Test begins <--\n");
         logger.info("Content of entry #100: " + cache.get(100) + "\n");
@@ -33,16 +81,13 @@ public class ReproducerTester {
         cache.put (100, new Book(100, "Alvaro", "Lopez", 1993));
         logger.info("Content of entry #100: " + cache.get(100) + "\n");
 
-        // This section will not work, as entry #100 is already there.
-        // We will retrieve the new value, but we will obtain many errors on the client side
-        logger.info("Put entry #100");
-        cache.put (100, new Book(100, "Alvaro", "Medina", 1993));
-        logger.info("Content of entry #100: " + cache.get(100) + "\n");
-
-
         return "";
     }
 
+    @GetMapping("remove-cache")
+    public String removeTestCache() throws Exception{
 
-
+        queriesCacheManager.getManager().administration().removeCache(cacheName);
+        return "";
+    }
 }
