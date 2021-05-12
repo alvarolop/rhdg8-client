3f2eccc71bc815537c5ed7cd04533a278a34e13a
Alvaro Lopez Medina <alopezme@redhat.com>
2021-05-12 15:33:55 +0200

    Update autoinstall

--- auto-install.sh
+++ auto-install.sh
@@ -31,24 +31,18 @@ else
 fi
 
 # Create RHDG Client configmap
-echo -e "\n[1/3]Creating client ConfigMap"
+echo -e "\n[1/2]Creating client ConfigMap"
 oc create configmap ${RHDG_CLIENT_NAME}-config \
 --from-file=application.properties=src/main/resources/application-k8s.properties \
 --from-file=logback-spring.xml=src/main/resources/logback-spring-k8s.xml -n $RHDG_NAMESPACE
 
 # Deploy the RHDG client
-echo -e "\n[2/3]Deploying the RHDG client"
+echo -e "\n[2/2]Deploying the RHDG client"
 oc process -f templates/rhdg-client.yaml -p APP_NAMESPACE=$RHDG_NAMESPACE -p APPLICATION_NAME=$RHDG_CLIENT_NAME -p GIT_REPOSITORY=$RHDG_GIT_REPO -p RHDG_CLUSTER_NAME=$RHDG_CLUSTER_NAME | oc apply -f -
-
-
-# Wait for BuildConfig
-echo -n "Waiting for build finished..."
-while [[ $(oc get pods -l app=$RHDG_CLIENT_NAME -n $RHDG_NAMESPACE -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
 
 # Wait for DeploymentConfig
 echo -n "Waiting for pods ready..."
 while [[ $(oc get pods -l app=$RHDG_CLIENT_NAME -n $RHDG_NAMESPACE -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
-
 
 
 RHDG_CLIENT_ROUTE=$(oc get routes $RHDG_CLIENT_NAME -n $RHDG_NAMESPACE --template='https://{{.spec.host}}')
