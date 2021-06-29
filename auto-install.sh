17ccb869ab01d4d24edc4decfd3e504c76db6650
Alvaro Lopez Medina <alopezme@redhat.com>
2021-06-29 16:01:01 +0200

    Adapt .properties to use ENV variables for auth and ssl. Adapt templates to set correct auth and ssl config. Adapt auto-install script to automate the process of deploying basic, auth, auth+ssl template versions

--- auto-install.sh
+++ auto-install.sh
@@ -7,6 +7,8 @@ RHDG_NAMESPACE=rhdg8
 RHDG_CLUSTER_NAME=rhdg
 RHDG_CLIENT_NAME=rhdg-client
 RHDG_GIT_REPO=https://github.com/alvarolop/rhdg8-client.git
+RHDG_AUTH_ENABLED=false
+RHDG_SSL_ENABLED=false
 
 #############################
 ## Do not modify anything from this line
@@ -19,7 +21,18 @@ echo -e " * RHDG_NAMESPACE: $RHDG_NAMESPACE"
 echo -e " * RHDG_CLUSTER_NAME: $RHDG_CLUSTER_NAME"
 echo -e " * RHDG_CLIENT_NAME: $RHDG_CLIENT_NAME"
 echo -e " * RHDG_GIT_REPO: $RHDG_GIT_REPO"
+echo -e " * RHDG_AUTH_ENABLED: $RHDG_AUTH_ENABLED"
+echo -e " * RHDG_SSL_ENABLED: $RHDG_SSL_ENABLED"
 echo -e "==============\n"
+
+if ! $RHDG_SSL_ENABLED; then
+    OCP_CLUSTER_TEMPLATE="rhdg-client"
+    SERVICE_MONITOR_HTTP_SCHEME="http"
+else
+    OCP_CLUSTER_TEMPLATE="rhdg-client-ssl"
+    SERVICE_MONITOR_HTTP_SCHEME="https"
+    RHDG_SECURITY="-u developer:developer"
+fi
 
 # Check if the user is logged in 
 if ! oc whoami &> /dev/null; then
@@ -27,13 +40,13 @@ if ! oc whoami &> /dev/null; then
     exit 1
 else
     echo -e "Check. You are correctly logged in. Continue..."
-    oc project default # To avoid issues with deleted projects
+    oc project rhdg8
 fi
 
 # Register Proto Schema
 echo -e "\n[1/3]Register Book Proto Schema"
-RHDG_SERVER_ROUTE=$(oc get routes ${RHDG_CLUSTER_NAME}-external -n $RHDG_NAMESPACE --template='http://{{.spec.host}}')
-curl -X POST $RHDG_SERVER_ROUTE/rest/v2/schemas/book.proto -d '// Generated from : com.alopezme.hotrodtester.configuration.BookSchema
+RHDG_SERVER_ROUTE=$(oc get routes ${RHDG_CLUSTER_NAME}-external -n $RHDG_NAMESPACE --template="${SERVICE_MONITOR_HTTP_SCHEME}://{{.spec.host}}")
+curl -X POST -k -v $RHDG_SECURITY $RHDG_SERVER_ROUTE/rest/v2/schemas/book.proto -d '// Generated from : com.alopezme.hotrodtester.configuration.BookSchema
 
 syntax = "proto2";
 
@@ -79,18 +92,20 @@ oc create configmap ${RHDG_CLIENT_NAME}-config \
 
 # Deploy the RHDG client
 echo -e "\n[3/3]Deploying the RHDG client"
-oc process -f templates/rhdg-client.yaml \
+oc process -f templates/${OCP_CLUSTER_TEMPLATE}.yaml \
     -p APP_NAMESPACE=$RHDG_NAMESPACE \
     -p APPLICATION_NAME=$RHDG_CLIENT_NAME \
     -p GIT_REPOSITORY=$RHDG_GIT_REPO \
-    -p RHDG_CLUSTER_NAME=$RHDG_CLUSTER_NAME | oc apply -f -
+    -p RHDG_CLUSTER_NAME=$RHDG_CLUSTER_NAME \
+    -p DATAGRID_AUTH_ENABLED=$RHDG_AUTH_ENABLED \
+    -p DATAGRID_SSL_ENABLED=$RHDG_SSL_ENABLED | oc apply -f -
 
 # Wait for DeploymentConfig
 echo -n "Waiting for pods ready..."
 while [[ $(oc get pods -l app=$RHDG_CLIENT_NAME -n $RHDG_NAMESPACE -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
 
 
-RHDG_CLIENT_ROUTE=$(oc get routes $RHDG_CLIENT_NAME -n $RHDG_NAMESPACE --template='http://{{.spec.host}}')
+RHDG_CLIENT_ROUTE=$(oc get routes $RHDG_CLIENT_NAME -n $RHDG_NAMESPACE --template="${SERVICE_MONITOR_HTTP_SCHEME}://{{.spec.host}}")
 
 echo -e "\RHDG Client information:"
 echo -e " * URL: $RHDG_CLIENT_ROUTE"
